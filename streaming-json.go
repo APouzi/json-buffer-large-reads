a2c5d76d537f986b3c3f4657537df2dd215d03d0
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-10 05:14:39 +0100

    moving file for streaming-json.go - AP

---
+++ streaming-json.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func StreaminJson() {
+	response, err := http.Get("https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json")
+
+	if err != nil {
+		panic(err)
+	}
+	decoder := json.NewDecoder(response.Body)
+	startDelim, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+	if startDelim != "[" {
+		fmt.Println(fmt.Errorf("expected `[' got %v"), startDelim)
+	}
+	count := 0
+	for decoder.More() {
+		if count == 20 {
+			break
+		}
+		movie := Movie{}
+		if err != nil {
+			fmt.Println("error")
+			panic(err)
+		}
+		decoder.Decode(&movie)
+
+		fmt.Println(movie)
+		count++
+	}
+}
